a4033c707a7d4fbfee8ee56b9645128f597f276a
Evgeny Garlukovich <evgeny.garlukovich@gmail.com>
2016-10-01 18:08:24 +0300

    [#16] Update `flag_icon` helper
    * Make `squared` argument keyword
    * Add documentation to method
    * Make refactoring
    * Update tests

--- lib/flag-icons-rails/rails/helpers.rb
+++ lib/flag-icons-rails/rails/helpers.rb
@@ -1,15 +1,29 @@
 module FlagIconsRails
   module Rails
     module ViewHelpers
-      def flag_icon(cca2, squared = false, html_options = {})
-        squared, html_options = false, squared if squared.is_a?(Hash)
-
-        content_class = "flag-icon flag-icon-#{cca2}"
-        content_class << " flag-icon-squared" if squared
-        content_class << " #{html_options[:class]}" if html_options.key?(:class)
-        html_options[:class] = content_class
+      # Helper to render flag icon
+      #
+      # @param [Symbol|String] country_code {https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2 ISO 3166-1 alpha-2}
+      #   country code
+      # @param [true, false] :squared Optional. It is used to determine squared or rectangle version
+      #   of flag will be rendered, defaults to +false+
+      # @param [Hash] html_options Optional. HTML options applied to rendered span, defaults to +{}+
+      #
+      # @return [String] +span+ with requested HTML options to display country flag
+      def flag_icon(country_code, squared: false, **html_options)
+        html_options[:class] = flag_icon_content_class(country_code, squared, html_options[:class])
 
         content_tag(:span, nil, html_options)
+      end
+
+      private
+
+      def flag_icon_content_class(country_code, squared, custom_css_class)
+        content_class = "flag-icon flag-icon-#{country_code.to_s}"
+        content_class << ' flag-icon-squared' if squared == true
+        content_class << " #{custom_css_class}" if custom_css_class
+
+        content_class
       end
     end
   end
--- lib/flag-icons-rails/version.rb
+++ lib/flag-icons-rails/version.rb
@@ -1,4 +1,4 @@
 module FlagIconsRails
-  VERSION = '1.1.2'.freeze
+  VERSION = '2.5.0'.freeze
   FLAG_ICONS_CSS_VERSION = '2.5.0'.freeze
 end
