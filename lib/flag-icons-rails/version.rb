730298f46a1b469be3918d0399f695b36feea0f3
Evgeny Garlukovich <evgeny.garlukovich@gmail.com>
2017-10-02 19:49:13 +0300

    [#24] Fix Rubocop offenses

--- lib/flag-icons-rails/version.rb
+++ lib/flag-icons-rails/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module FlagIconsRails
-  VERSION = '2.5.0'.freeze
-  FLAG_ICONS_CSS_VERSION = '2.5.0'.freeze
+  VERSION = '2.5.0'
+  FLAG_ICONS_CSS_VERSION = '2.5.0'
 end
