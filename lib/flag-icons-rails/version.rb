5e082d180ce22de82baa62bb35b813bf8f8fa05f
Niklas Bichinger <niklas@bichinger.de>
2020-01-16 18:30:32 +0100

    update version to 3.4.5

--- lib/flag-icons-rails/version.rb
+++ lib/flag-icons-rails/version.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
 module FlagIconsRails
-  VERSION = '3.1.0'.freeze
-  FLAG_ICONS_CSS_VERSION = '3.1.0'.freeze
+  VERSION = '3.4.5'
+  FLAG_ICONS_CSS_VERSION = '3.4.5'
 end
